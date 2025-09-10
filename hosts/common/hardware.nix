2b0b921b9b5cd29af0452c430889caac0d7a54c6
Possiblyai <imrana50280@gmail.com>
2025-09-10 10:28:24 -0400

    Fixed nvidia drivers issue

--- hosts/common/hardware.nix
+++ hosts/common/hardware.nix
@@ -1,9 +1,9 @@
-_: {
-  hardware = {
+_: { hardware = {
     # Enable hardware accelerated graphics drivers.
     graphics.enable = true;
 
     nvidia = {
+      open = false;  
       modesetting.enable = true;
       nvidiaSettings = true;
     };
