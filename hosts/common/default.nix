0e813e208efa5ec28b3853ae6f09e0c03bb99590
Possiblyai <imrana50280@gmail.com>
2025-09-10 10:28:05 -0400

    Prevent shutoff on screenclose and add personal devices as authorized keys

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./boot.nix
     ./hardware.nix
+    ./logind.nix
     ./networking.nix
     ./nix-settings.nix
     ./openssh.nix
@@ -15,6 +16,10 @@
       isNormalUser = true;
       description = "Cluster User";
       extraGroups = ["networkmanager" "wheel"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMD7R3C50biFqJaBhIXYQZNCKtsM/e35Oh7b1h6ESSX ai@ai-duo"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGdhbEfOlA2Q4y1OHY4MdFOkcQpuZzJKaPxqFFsyngHM ai@ai-desk"
+      ];
     };
   };
 }
