014b19afd070e3431a43ee8678b75d1f4026f445
Possiblyai <imrana50280@gmail.com>
2025-10-06 11:16:24 -0400

    New network config for worker-2

--- hosts/worker-2/default.nix
+++ hosts/worker-2/default.nix
@@ -8,14 +8,21 @@
 
   # Define hostname.
   networking = {
-    hostName = "worker";
-    interfaces.enp0s31f6 = {
-      ipv4.addresses = [
-        {
-          address = "10.0.0.3";
-          prefixLength = 24;
-        }
-      ];
+    hostName = "worker-2";
+    networkmanager.ensureProfiles.profiles.local_ethernet = {
+      connection = {
+        id = "local_ethernet";
+        interface-name = "enp0s20f0u2";
+        timestamp = "1759613893";
+        type = "ethernet";
+        uuid = "2d2bdf35-5b3e-471c-9b0a-549ce8506682";
+      };
+      ethernet = {};
+      ipv4 = {
+        address1 = "10.0.0.82/24";
+        method = "manual";
+      };
+      proxy = {};
     };
   };
 
